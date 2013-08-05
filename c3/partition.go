c044c5e772b5e898fad07edd1c8a4c23eb9b0d2c
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2013-08-05 14:58:54 +0000

    3.7.4: Partitioning

---
+++ c3/partition.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+type (
+	Keyfunc  func(...interface{}) string
+	Memfunc  func(...interface{}) interface{}
+	Algofunc func(int, []int) []int
+)
+
+func memoize(f Memfunc, kf Keyfunc) Memfunc {
+	if kf == nil {
+		kf = func(v ...interface{}) string {
+			// Very naive keygen func, simply use string representation
+			// of every param
+			var buffer bytes.Buffer
+			for i := 0; i < len(v); i++ {
+				buffer.WriteString(fmt.Sprint(v[0]))
+				buffer.WriteString(",")
+			}
+			return buffer.String()
+		}
+	}
+
+	cache := make(map[string]interface{})
+	return func(v ...interface{}) interface{} {
+		cachekey := kf(v)
+		if r, ok := cache[cachekey]; ok {
+			// Hit, return previous result
+			return r
+		}
+		// Miss, call calculator
+		r := f(v...)
+		cache[cachekey] = r
+		return r
+	}
+}
+
+func findshare(mf Memfunc) Algofunc {
+
+	f := func(target int, treasures []int) []int {
+		if target == 0 {
+			return make([]int, 0)
+		}
+		if target < 0 || len(treasures) == 0 {
+			return nil
+		}
+		first, rest := treasures[0:1], treasures[1:]
+		solution := toIntSlice(mf(target-first[0], rest))
+		if solution != nil {
+			return append(first, solution...)
+		}
+		return toIntSlice(mf(target, rest))
+	}
+
+	if mf == nil {
+		mf = func(v ...interface{}) interface{} {
+			return f(toInt(v[0]), toIntSlice(v[1]))
+		}
+	}
+	return f
+}
+
+func main() {
+	treasure := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	target := 209
+
+	rawfindshare := findshare(nil)
+	duration, res := timeThis(rawfindshare, target, treasure)
+	fmt.Printf("Raw partition %v into %d: %d at %s\n", treasure, target, res, duration)
+
+	// Construct memoize partitioner
+	mfindshare := func() Algofunc {
+		// Forward decleration of calculator
+		var calcshare Algofunc
+
+		// Memoizer
+		memfindshare := memoize(func(values ...interface{}) interface{} {
+			return calcshare(toInt(values[0]), toIntSlice(values[1]))
+		}, nil)
+
+		// Construct the calculator, use the memonizer defined above
+		calcshare = findshare(memfindshare)
+
+		// Return a function that will do the neccessary typecasting
+		return func(target int, treasures []int) []int {
+			return toIntSlice(memfindshare(target, treasures))
+		}
+	}()
+
+	duration, res = timeThis(mfindshare, target, treasure)
+	fmt.Printf("Memoized partition %v into %d: %d at %s\n", treasure, target, res, duration)
+
+}
+
+// Utility funcations
+func toInt(v interface{}) int {
+	if value, ok := v.(int); ok {
+		return value
+	}
+	panic(fmt.Sprintf("Not an int value: %v", v))
+}
+
+func toIntSlice(v interface{}) []int {
+	if value, ok := v.([]int); ok {
+		return value
+	}
+	panic(fmt.Sprintf("Not an int slice value: %v", v))
+}
+
+func timeThis(f Algofunc, param1 int, param2 []int) (time.Duration, []int) {
+	start := time.Now()
+	res := f(param1, param2)
+	return time.Since(start), res
+}
---
+++ c3/partition.md
@@ -0,0 +1,26 @@
+The point of making a generic memoize is to be able to use it with different algorithms. The *partition* algorithm from [1.8: Partitioning](http://higherordergo.blogspot.com/2013/07/18-partitioning.html) has a different signature than the fibonacci algorithm yet we can use [*memoizer*](http://higherordergo.blogspot.com/2013/08/35-memoize-module_4.html) unchanged. First for the type definitions, the only difference being the signature of the *Algofunc*:
+
+CODE
+
+*memoize* is exactly the same as before:
+
+CODE
+
+The only change we need to make to *findshare* is to change it to a constructor that accepts a *Memfunc* and returns the actual algorithm. It is the same patterns as we used for *fib*:
+
+CODE
+
+In the *main* function we start with doing unmemoized calculations:
+
+CODE
+
+Then we proceed to do the memoized calculations on the same data, constructing the same typecasting code structure as before:
+
+CODE
+
+The speedup we gain for the memoization is about 96% for this example:
+
+	Raw partition [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20] into 209: [2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20] at 558.018056ms
+	Memoized partition [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20] into 209: [2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20] at 18.783834ms
+
+Get the source at [GitHub](https://github.com/mg/hog/blob/master/c3/partition.md).
