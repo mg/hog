5ef16d291c759d1374bed93e1fee21ae90fdf3fa
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2013-08-06 20:07:52 +0000

    3.8.1: Memoization of Object Methods

---
+++ c3/method.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+type (
+	Keyfunc  func(...interface{}) string
+	Memfunc  func(...interface{}) interface{}
+	Algofunc func(int) int
+)
+
+func memoize(f Memfunc, kf Keyfunc) Memfunc {
+	if kf == nil {
+		kf = func(v ...interface{}) string {
+			// Very naive keygen func, simply use string representation
+			// of every param
+			var buffer bytes.Buffer
+			for i := 0; i < len(v); i++ {
+				buffer.WriteString(fmt.Sprint(v[0]))
+				buffer.WriteString(",")
+			}
+			return buffer.String()
+		}
+	}
+
+	cache := make(map[string]interface{})
+	return func(v ...interface{}) interface{} {
+		cachekey := kf(v)
+		if r, ok := cache[cachekey]; ok {
+			// Hit, return previous result
+			return r
+		}
+		// Miss, call calculator
+		r := f(v...)
+		cache[cachekey] = r
+		return r
+	}
+}
+
+type Object struct {
+}
+
+func (o *Object) Say(p int) int {
+	time.Sleep(1 * time.Second)
+	return p
+}
+
+func main() {
+	var obj Object
+	duration, res := timeThis(obj.Say, 1)
+	fmt.Printf("Object says: %d, takes %s to do it.\n", res, duration)
+
+	// Construct memoize partitioner
+	msay := func(f Algofunc) Algofunc {
+		memsay := memoize(func(values ...interface{}) interface{} {
+			return f(toInt(values[0]))
+		}, nil)
+
+		// Return a function that will do the neccessary typecasting
+		return func(val int) int {
+			return toInt(memsay(val))
+		}
+	}(obj.Say)
+
+	duration, res = timeThis(msay, 1)
+	fmt.Printf("Memoized object says: %d, takes %s to do it.\n", res, duration)
+	duration, res = timeThis(msay, 1)
+	fmt.Printf("Memoized object says: %d, takes %s to do it.\n", res, duration)
+	duration, res = timeThis(msay, 2)
+	fmt.Printf("Memoized object says: %d, takes %s to do it.\n", res, duration)
+	duration, res = timeThis(msay, 2)
+	fmt.Printf("Memoized object says: %d, takes %s to do it.\n", res, duration)
+}
+
+// Utility funcations
+func toInt(v interface{}) int {
+	if value, ok := v.(int); ok {
+		return value
+	}
+	panic(fmt.Sprintf("Not an int value: %v", v))
+}
+
+func timeThis(f Algofunc, param int) (time.Duration, int) {
+	start := time.Now()
+	res := f(param)
+	return time.Since(start), res
+}
---
+++ c3/method.md
@@ -0,0 +1,25 @@
+Our final example from chapter three is the memoization of object methods. Again we can use the same [*memoizer*](http://higherordergo.blogspot.com/2013/08/35-memoize-module_4.html) function of type *Memfunc* as before, no changes are needed.
+
+CODE
+
+Our object is a trivial one, its only purpose to demonstrate the speedup from the memoizing transformation. The method accepts an int argument, waits for one second and then returns the argument.
+
+CODE
+
+The *main* function starts by executing the raw method and timing it:
+
+CODE
+
+Our typecasting code structure is simpler this time around because the method is not a recursive one, only typecasting is needed.
+
+CODE
+
+The results are predictable, the method takes one second to execute while the memoized version drops down to 15ms after the first execution:
+
+	Object says: 1, takes 1.001060856s to do it.
+	Memoized object says: 1, takes 1.001117812s to do it.
+	Memoized object says: 1, takes 14.71us to do it.
+	Memoized object says: 2, takes 1.001044387s to do it.
+	Memoized object says: 2, takes 14.893us to do it.
+
+Get the source at [GitHub](https://github.com/mg/hog/blob/master/c3/method.md).
